70c6b55a12b33448d3682f76ecc7e123ad5631b5
lyd1amc <lydiafernandinha@gmail.com>
2025-09-30 12:05:26 -0300

    Add files via upload

---
+++ script2.js
@@ -0,0 +1,5 @@
+//arrays -> vetores, matrizes
+// estrutura de dados para armazenar um conjunto de elementos
+
+let frutas = ["Banana", "Maçã", "Melancia"]; //array de tamanho 3
+console.log(frutas);
